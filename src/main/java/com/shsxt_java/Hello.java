509f0e855e56110d31fba161691c0557fda2447b
zbb775492752 <775492752@qq.com>
2018-04-12 17:16:09 +0800

    first commit

---
+++ src/main/java/com/shsxt_java/Hello.java
@@ -0,0 +1,10 @@
+package com.shsxt_java;
+
+/**
+ * Created by Administrator on 2018/4/12 0012.
+ */
+public class Hello {
+    public static void hello(){
+        System.out.println("nihao,李鹏飞");
+    }
+}
